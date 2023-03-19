fa87eda52ec468854197b7db768b5dcbe497deeb
carlobisda <38801646+carlobisda@users.noreply.github.com>
2023-03-19 01:18:25 -0400

    vroom vroom

---
+++ matrixEffect.py
@@ -0,0 +1,52 @@
+import os
+import time
+import random
+
+stop_loop = False
+
+os.system('color 0a')
+
+nums = [1,0]
+
+while not stop_loop:
+    print(random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),)
+
+    time.sleep(0.15)
---
+++ readMe.md
@@ -0,0 +1,14 @@
+
+Name: Matrix Effect
+Version: 1.1.0
+Author: Carlo Bisda
+License: MIT
+
+Crude CLI replication of the matrix code effect from the movie fanchise, "The Matrix".
+Open application on Windows prompt and l33t haxor skills will recieve 5% boost.
+
+1.1.0
+- added a loop feature
+
+1.0.0
+- original program created
